2117ec8148db4629d1711c1f813cd9255b80a650
Navnath-M <163994091+Navnath-M@users.noreply.github.com>
2025-04-07 19:30:49 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-'# print("Hello from Jenkins and Python!")' 
+length = 5
+width = 3
+
+# Calculate the area
+area = length * width
+
+# Print the result
+print(f"The area of the rectangle with length {length} and width {width} is: {area} square units")
